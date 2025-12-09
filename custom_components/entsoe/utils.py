59c0c4913498a7339e89d531f2bdf00fc08e9f0d
K2 <115832286+KriVaTri@users.noreply.github.com>
2025-12-09 13:34:02 +0100

    Add utility functions for time interval handling
    Added functions to convert ISO 8601 duration to minutes and calculate bucket time.

--- custom_components/entsoe/utils.py
+++ custom_components/entsoe/utils.py
@@ -1,17 +1,22 @@
-get_energy_prices:
-  fields:
-    config_entry:
-      required: true
-      selector:
-        config_entry:
-          integration: entsoe
-    start:
-      required: false
-      example: "2023-01-01 00:00:00"
-      selector:
-        datetime:
-    end:
-      required: false
-      example: "2023-01-01 00:00:00"
-      selector:
-        datetime:
+import re
+from datetime import timedelta
+
+
+def get_interval_minutes(iso8601_interval: str) -> int:
+    """
+    Convert an ISO 8601 duration string to total minutes.
+    Example: 'PT15M' -> 15
+    """
+
+    return int(re.match(r"PT(\d+)M", iso8601_interval).group(1))
+
+
+def bucket_time(ts, bucket_size):
+    """
+    Get the bucket time for the interval.
+
+    e.g. for a bucket size of 15 minutes, the time 10:07 would be rounded down to 10:00,
+    """
+    return ts - timedelta(
+        minutes=ts.minute % bucket_size, seconds=ts.second, microseconds=ts.microsecond
+    )
